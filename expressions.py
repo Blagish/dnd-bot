f81a59751e1b9eeb96659636922476663c01b921
snarktocrab <danysor@gmail.com>
2019-11-09 21:09:48 +0300

    added proper advantage/disadvantage parsing & multiple commands can be used in one message

--- expressions.py
+++ expressions.py
@@ -4,7 +4,16 @@ from random import randint
 # pattern = '/roll\s+(?P<num>\d*)d(?P<dice>\d*)\s*\+\s*(?P<mod>\d*)'
 command = 'roll\s+'
 # command = '/roll\s+'
-dice = '([\+\-]?)\s*(?P<num>\d*)d(?P<dice>\d*)'
+dice = '''([\+\-]?)     # check sign
+        \s*             # remove whitespace
+        (?:             # find either
+        (?P<num>\d*)    # number of dice to roll
+        |               # or
+        (?P<adv>[ad])   # whether the roll is made with advantage
+        )
+        d               # any roll must contain a 'd' character
+        (?P<dice>\d+)   # and the type of dice to roll
+        '''
 modifiers = '([\+\-])\s*(?P<mod>\d+)(?!d)'
 
 
@@ -12,21 +21,33 @@ modifiers = '([\+\-])\s*(?P<mod>\d+)(?!d)'
 
 def d(s):
     res = 0
-    print(re.findall(dice, s))
+    print("matched dice:", re.findall(dice, s, flags = re.X))
     output = ''
     output += 'Rolling\n'
-    for roll in re.findall(dice, s):
+    SIGN, NUM, ADV, DICE = 0, 1, 2, 3
+    for roll in list(map(list, re.findall(dice, s, flags = re.VERBOSE))):
         mul = 1
-        if roll[1] == '':
-            roll = (roll[0], '1', roll[2])
-        for i in range(int(roll[1])):
+        if roll[NUM] == '':
+            roll[NUM] = '1'
+        print(roll)
+        for i in range(int(roll[NUM])):
             if roll[0] == '-':
                 mul = -1
-                output += "- ["
+                output += "- "
             else:
-                output += "+ ["
-            r = randint(1, int(roll[2]))
-            output += f'{r}] '
+                output += "+ "
+            output += roll[ADV] + '['
+            if roll[ADV] == '':
+                r = randint(1, int(roll[DICE]))
+                output += f'{r}] '
+            else:
+                r1 = randint(1, int(roll[DICE]))
+                r2 = randint(1, int(roll[DICE]))
+                output += f'{r1}|{r2}] '
+                if roll[ADV] == 'a':
+                    r = max(r1, r2)
+                else:
+                    r = min(r1, r2)
             res += r * mul
 
     for mod in re.findall(modifiers, s):
