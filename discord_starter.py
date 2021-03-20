e5d5a07f6bd9396286fde0201be896d12562df0f
blagish <sir.blagish@gmail.com>
2021-03-21 01:25:18 +0300

    more secure token rework

--- discord_starter.py
+++ discord_starter.py
@@ -1,5 +1,5 @@
 import discord
-from config import token_discord
+import os
 from random import randint, choice
 import server
 
@@ -7,11 +7,9 @@ import server
 client = discord.Client()
 
 danika_react = ['а?', 'Я тут!', 'Меня звали?', 'Что-то нужно?']
-daniel_react = ['О, вы знакомы с моим братом', 'Хоть мы и близнецы, но я немного младше.',
-                'Многие говорят, что мой брат похож на какого-то Страда... кто это?',
-                'У Даниеля есть огромная коллекция ножей. Он любит их точить.', 'Не хочу показаться грубой, но давайте не будем о нем.']
 yeno = ['Ага', 'Неа']
 
+discord_id = os.environ.get('DISCORD_ID')
 
 @client.event
 async def on_message(message):
@@ -23,14 +21,12 @@ async def on_message(message):
         output = server.process(text)
         for msg in output:
             await message.channel.send(msg)
-    elif '684755387408580623' in text:
+    elif discord_id in text:
         await message.channel.send(choice(danika_react+[':eyes: '*randint(1, 3)]))
     elif 'даник' in text.lower():
         if '?' in text:
             await message.channel.send(choice(yeno))
         elif randint(1, 10) == 10:
             await message.channel.send(choice(danika_react))
-##    elif 'Даниел' in text or 'Даниэл' in text:
-##        if randint(1, 10) == 10:
-##            await message.channel.send(choice(daniel_react))
-client.run(token_discord)
+
+client.run(os.environ.get('DISCORD_TOKEN'))
