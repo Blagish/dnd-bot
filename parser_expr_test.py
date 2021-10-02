e088de988652125390d9bd2249a310f304556752
blagish <sir.blagish@gmail.com>
2021-10-03 03:08:48 +0400

    added damage types; reworked comma operations; added ops with comma lists and something else i forgor

--- parser_expr_test.py
+++ parser_expr_test.py
@@ -9,12 +9,12 @@ t_DIV = r'\/'
 t_VAL = r'[0-9]+'
 t_DOT = r'\.'
 t_VAR = r'it'
-t_COMMENT = r'q'#r'[a-zA-Zа-яА-ЯёЁ][a-zA-Zа-яА-ЯёЁ]+'
 t_LBRACKET = r'\('
 t_RBRACKET = r'\)'
 t_SLBRACKET = r'\['
 t_SRBRACKET = r'\]'
 t_COMMA = r'\,'
+t_SEMICOLON = r'\;'
 t_FOR = r'x'
 t_DIE = r'd'
 t_ADVDIE = r'ad'
@@ -28,6 +28,8 @@ t_SUM = r'sum'
 t_BIGGER = r'\>'
 t_LESSER = r'\<'
 t_EQUAL = r'\='
+t_NOTEQUAL1 = r'≠'
+t_NOTEQUAL2 = r'\!\='
 t_BIGGEREQUAL = r'\>\=|\=\>'
 t_LESSEREQUAL = r'\<\=|\=\<'
 t_IF = '\?'
@@ -36,18 +38,31 @@ t_ARG = r'gwf|ea|rt|st'
 t_ignore = ' \n\t'
 
 
+def t_COMMENT(t):
+    r"""[a-zA-Zа-яА-ЯёЁ]+"""
+    if t.value in function_keywords:  # is this a keyword
+        t.type = function_keywords[t.value]
+    return t
+
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
 
-lex.lex()
+lexer = lex.lex()
 parser = yacc.yacc()
 
 
 def parse(expression):
     result = parser.parse(expression)
     return result
+
+
+def test(expression):
+    lexer.input(expression)
+    for tok in lexer:
+        print(tok)
 
 
 def d2(expression):
@@ -58,6 +73,6 @@ def d2(expression):
     expression = expression.replace('к', 'k')
     res = parse(expression)
     ans, sol = res.calculate()
-    if type(sol) == type(tuple()):
-        sol = ', '.join(sol)
+    if type(sol) == list:
+        sol = '('+', '.join(sol)+')'
     return sol, ans
