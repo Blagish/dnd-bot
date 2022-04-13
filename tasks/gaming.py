c2e5414705f519dfec3646cc27aa00a291c3da62
Kirill <sir.blagish@gmail.com>
2022-04-14 02:22:30 +0400

    await gaming

--- tasks/gaming.py
+++ tasks/gaming.py
@@ -8,17 +8,16 @@ class Gaming(commands.Cog, name='Гейминг'):
         self.bot = bot
         self.game = None
         self.games = ('D&D 5e', 'Pathfinder 2e', 'Fate', 'City of Mist', 'Prowlers & Paragons', 'Minecraft')
-        self.start_new_game.start()
+        await self.start_new_game.start()
 
     def cog_unload(self):
-        self.start_new_game.cancel()
+        await self.start_new_game.cancel()
 
     @tasks.loop(hours=8)
     async def start_new_game(self):
         game_title = self.choose_a_game()
         self.game = discord.Game(name=game_title)
         print(f'Starting playing {game_title}')
-        print(type(self.bot), self.bot)
         await self.bot.change_presence(activity=self.game)
 
     def choose_a_game(self):
