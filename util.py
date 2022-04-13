229d68d25a99c3f8ebf7e864cf205d880f3e430f
Kirill <sir.blagish@gmail.com>
2022-04-13 22:43:36 +0400

    oops

--- util.py
+++ util.py
@@ -3,15 +3,14 @@ MAX_MESSAGE_LENGTH = 1999
 
 async def ctx_send(ctx, message):
     messages = [message]
-    while len(messages[-1]) > MAX_MESSAGE_LENGTH:
-        messages = []
+    while len(message := messages[-1]) > MAX_MESSAGE_LENGTH:
         enter = message.rfind('\n')
         while enter >= MAX_MESSAGE_LENGTH:
             enter = message.rfind('\n', 0, enter)
         if enter == -1:
             enter = MAX_MESSAGE_LENGTH
-        messages.append(messages[-1][enter:])
-        messages[-2] = messages[-2][:enter]
+        messages.append(message[enter:])
+        messages[-2] = message[:enter]
 
     for m in messages:
         await ctx.send(m)
