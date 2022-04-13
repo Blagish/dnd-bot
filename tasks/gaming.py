be4a259ad6986b2459bad0ddad8c589d3df65e81
Kirill <sir.blagish@gmail.com>
2022-04-14 02:37:01 +0400

    .danika gaming.

--- tasks/gaming.py
+++ tasks/gaming.py
@@ -7,7 +7,6 @@ class Gaming(commands.Cog, name='Гейминг'):
     def __init__(self, bot):
         self.bot = bot
         self.games = ('D&D 5e', 'Pathfinder 2e', 'Fate', 'City of Mist', 'Prowlers & Paragons', 'Minecraft')
-        self.start_new_game.before_loop(self.bot.wait_until_ready())
         self.start_new_game.start()
 
     def cog_unload(self):
@@ -18,6 +17,11 @@ class Gaming(commands.Cog, name='Гейминг'):
         game_title = self.choose_a_game()
         await self.bot.change_presence(activity=discord.Game(name=game_title))
         print(f'Starting playing {game_title}')
+
+    @start_new_game.before_loop
+    async def before_start_new_game(self):
+        print('waiting...')
+        await self.bot.wait_until_ready()
 
     def choose_a_game(self):
         return choice(self.games)
