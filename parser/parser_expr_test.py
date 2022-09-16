a5fe811a46ef83c4a2f8951f1f341dad96faa3e3
Kirill <sir.blagish@gmail.com>
2022-09-16 23:35:02 +0300

    revert

--- parser/parser_expr_test.py
+++ parser/parser_expr_test.py
@@ -1,7 +1,6 @@
 from .ply_data import *
 import ply.lex as lex
 import ply.yacc as yacc
-from .comments import replace, replace_back
 
 t_ADD = r'\+'
 t_SUB = r'-'
@@ -68,11 +67,8 @@ def test(expression):
 
 
 def d2(expression):
-    dict_, expression = replace(expression)
-    print(dict_)
     res = parse(expression)
     ans, sol = res.calculate()
     if type(sol) == list:
         sol = '('+', '.join(sol)+')'
-    ans, sol = replace_back(dict_, ans), replace_back(dict_, sol)
     return sol, ans
