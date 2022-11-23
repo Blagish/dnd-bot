8af14ee908015d0be3c346e97462afa59950f16a
Kirill <sir.blagish@gmail.com>
2022-11-23 21:21:27 +0300

    dice desc + card fix

--- commands/misc.py
+++ commands/misc.py
@@ -1,10 +1,10 @@
 from random import choice, randint
 
-from discord import Embed, Colour, ButtonStyle
+from discord import Embed, Colour, ButtonStyle, File
 from discord.ext import commands
 
 
-class Cards(commands.Cog, name='Игральные карты'):
+class Cards(commands.Cog, name='Разное: игральные карты'):
     def __init__(self, bot):
         self.bot = bot
         self.cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
@@ -22,11 +22,23 @@ class Cards(commands.Cog, name='Игральные карты'):
     @staticmethod
     def gen_embed_picture(title, name, color):
         e = Embed(title=title, colour=color)
-        e.set_image(url=f'cards/{name}.svg')
-        return e
+        file = File(f"cards/{name}.svg", filename=f"card_{name}.svg")
+        e.set_image(url=f"attachment://card_{name}.svg")
+        return file, e
+
+    @commands.command(name='card52', aliases=['52', 'карт52', 'к52', 'k52', 'c52'])
+    async def card52(self, ctx):
+        """Случайным образом вытягивает одну карту из стандартной колоды в 52 карты."""
+        card, suit = choice(self.cards), choice(self.suits)
+        name = f"{card}{suit}"
+        color = self.colors[suit]
+        title = f"{self.full_cards.get(card, card)} of {self.full_suits[suit]}"
+        file, embed = self.gen_embed_picture(title, name, color)
+        await ctx.send(file=file, embed=embed)
 
     @commands.command(name='card54', aliases=['54', 'карт54', 'к54', 'k54', 'c54'])
     async def card54(self, ctx):
+        """Случайным образом вытягивает одну карту из стандартной колоды в 52 карты + 2 джокера."""
         if randint(1, 54) < 3:  # Jokers:
             name = choice(self.jokers)
             color = self.colors[name]
@@ -36,4 +48,5 @@ class Cards(commands.Cog, name='Игральные карты'):
             name = f"{card}{suit}"
             color = self.colors[suit]
             title = f"{self.full_cards.get(card, card)} of {self.full_suits[suit]}"
-        await ctx.send(embed=self.gen_embed_picture(title, name, color))
+        file, embed = self.gen_embed_picture(title, name, color)
+        await ctx.send(file=file, embed=embed)
